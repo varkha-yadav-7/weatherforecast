090ef9ea0f2f35ac56a84b14c5c6cf63d2a52ee5
Varkha Yadav <varkhay@gmail.com>
2020-04-28 22:52:51 +0530

    Static root added

--- weatherforecast/settings.py
+++ weatherforecast/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,8 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+PROJECT_DIR=os.path.dirname(__file__)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT=os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
